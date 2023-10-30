cf089910b37c7ade1f511b424b86b22600c786f5
pauljay <ceej5932@gmail.com>
2023-10-30 12:51:49 +0000

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by Paul Jay on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
